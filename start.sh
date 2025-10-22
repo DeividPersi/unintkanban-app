9680d9ac811ee8f3e069ce963b6d63f5c9780e21
MAGDEIVID <magdeivid@outlook.com>
2025-10-21 21:37:18 -0300

    feat: primeiro commit

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Start Django backend
+python manage.py migrate
+python manage.py collectstatic --noinput
+python manage.py runserver 0.0.0.0:8000 &
+BACKEND_PID=$!
+
+# Start nginx
+nginx -g "daemon off;" &
+NGINX_PID=$!
+
+# Wait for any process to exit
+wait $BACKEND_PID $NGINX_PID
